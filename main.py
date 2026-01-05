5ce51118ac953ae1644e5582d5a0e11cdcf210ee
vortimond <161199587+vortimond@users.noreply.github.com>
2026-01-06 00:16:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import random
 
 planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-selected_planet = random.choice(planets)
+random_planet = random.choice(planets)
+random_number = planets.index(random_planet)
 print("I have selected a planet from eight possible options:")
 print(planets)
 while True:
-    user_input = input("Tell me which one [q to quit]: ")
-    if user_input == "q":
-        break
-    elif user_input == selected_planet:
-        print("Correct!")
+    user_planet = input("Tell me which one [q to quit]: ")
+    user_planet = user_planet.capitalize()
+    if user_planet in planets:
+        user_number = planets.index(user_planet)
+        if random_number == user_number:
+            print("Correct!")
+            break
+        elif random_number > user_number:
+            print("The planet is farther away from the Sun.")
+        else:
+            print("The planet is closer to the Sun.")
+    elif user_planet == "Q":
         break
